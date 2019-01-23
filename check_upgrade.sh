01e3988e7e431b7b0ee64861c354b8ca504d312b
azlux <github@azlux.fr>
2019-01-23 15:29:06 +0100

    Remove useless echo

--- check_upgrade.sh
+++ check_upgrade.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+
 packages_to_upgrade=()
 regex="The following packages will be upgraded:\s(.*)[0-9]+ upgraded, [0-9]+ newly installed, [0-9]+ to remove"
+
 check_update () {
     sudo lxc-attach -n $1 -- apt-get update -qq
     res=$(sudo lxc-attach -n $1 -- apt-get upgrade --show-upgraded --assume-no)
@@ -10,13 +12,12 @@ check_update () {
     fi
 }
 
-# Upgrade all active containers
+# Check upgrades for all active containers
 for cx in `sudo lxc-ls --active --line`; do
     check_update $cx
 done
 
-echo $packages_to_upgrade
-
+# Check upgrade for the Host
 apt-get update -qq
 res=$(apt-get upgrade --show-upgraded --assume-no)
 res="${res//[$'\t\r\n']}"
@@ -24,6 +25,7 @@ if [[ "$res" =~ $regex ]]; then
     packages_to_upgrade+=${BASH_REMATCH[1]}
 fi
 
+# Send email to root if there are upgrades available
 if [ ! -z "$packages_to_upgrade" ]; then
     res=$(echo $packages_to_upgrade | tr ' ' '\n' | sort | uniq -c)
     echo -e "Packages waiting for upgrade\n\n$res" | mail -s "Proxmox upgrade list" root
